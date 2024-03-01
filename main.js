19a6db519330514aa7b43b01633149618bfc3110
LeRo <stilov94@gmail.com>
2024-03-02 01:54:49 +0500

    up

--- main.js
+++ main.js
@@ -3,16 +3,19 @@ const inputButton = document.getElementById("inputButton");
 const taskList = document.getElementById("taskList");
 const emptySheet = document.getElementById("emptySheet");
 
-window.onload = () => inputButton.addEventListener("click", AddTask);
-
 function AddTask() {
     if (emptySheet) {
         emptySheet.remove();
         taskList.style.alignItems = "none";
     }
     taskList.appendChild(FormElement());
+
     SetTaskText();
     inputText.value = "";
+}
+
+function RemoveTask(task) {
+    task.target.remove();
 }
 
 function FormElement() {
@@ -20,7 +23,7 @@ function FormElement() {
     div.setAttribute("class", "task");
     div.setAttribute("id", "task");
     div.innerHTML = `<p class="task__title"></p>
-    <button class="task__button">
+    <button class="task__button" id="taskButton">
         <svg width="30" height="30" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M24.797 6.984H5.203M12.328 12.328v7.125M17.672 12.328v7.125M23.016 6.984v16.922a.89.89 0 0 1-.891.89H7.875a.89.89 0 0 1-.89-.89V6.984M19.453 6.984v-1.78a1.781 1.781 0 0 0-1.781-1.782h-5.344a1.781 1.781 0 0 0-1.781 1.781v1.781"
@@ -38,3 +41,6 @@ function SetTaskText() {
     const taskText = document.querySelectorAll(".task__title");
     taskText[taskText.length - 1].innerText = inputText.value;
 }
+
+window.onload = () => inputButton.addEventListener("click", AddTask);
+taskList.addEventListener("click", RemoveTask, false);
